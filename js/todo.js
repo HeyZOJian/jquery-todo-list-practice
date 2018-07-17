5a9268bbb2280a008b39863495f5237c8a19b4a6
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-17 17:39:23 +0800

    Ancient implementation

--- js/todo.js
+++ js/todo.js
@@ -12,14 +12,47 @@ $(document)
                 if (i === 8 || i === 12 || i === 16 || i === 20) {
                     uuid += '-';
                 }
-                uuid += (i === 12
-                    ? 4
-                    : (i === 16
-                        ? (random & 3 | 8)
-                        : random)).toString(16);
+                uuid += (i === 12 ?
+                    4 :
+                    (i === 16 ?
+                        (random & 3 | 8) :
+                        random)).toString(16);
             }
             return uuid;
         }
 
-        // code to be implemented
+        // add todo item
+
+        $("#button").click(function () {
+            var todoItem = $("input[name=ListItem]").val();
+            // console.log(todoItem);
+            todoItem = "<li id=" + generateUUID() + " class=\"\">" +
+                "<input name=\"done-todo\" type=\"checkbox\" class=\"done-todo\">" + todoItem + "</li>"
+            $("ol").append(todoItem);
+        })
+
+        // switch todo item status
+        $(document).on("click", ".done-todo", function () {
+            $(this).parent().toggleClass("checked");
+        });
+
+        // modify todo item
+        $(document).on("click", "li", function () {
+            var html = $(this).html().toString();
+            var todoItemText = html.substr(html.indexOf(">"),html.length)
+            // console.log(todoItemText);
+            // $(this).replaceWith()
+            $(this).attr('contentEditable', 'true');
+        });
+
+        // show all undone items
+        $(".active").click(function () {
+            $("li.checked").css("display", "none");
+        })
+
+        // show all items
+        $(".all").click(function () {
+            $("li.checked").css("display", "");
+        })
+
     });
